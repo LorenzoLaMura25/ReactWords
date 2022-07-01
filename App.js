177d9f0653527e74002e73207c658caa8371e83d
LorenzoLaMura25 <107468950+LorenzoLaMura25@users.noreply.github.com>
2022-07-01 08:32:29 +0200

    Add files via upload
    Words Board without functionality

---
+++ App.js
@@ -0,0 +1,119 @@
+import React,{useState} from 'react';
+import './App.css';
+
+
+function App() {
+
+
+
+ 
+
+
+  return (
+    <div className="App">
+      <div className="row-one">
+      <div className="first"></div>
+      <div className="first"></div>
+      <div className="first"></div>
+      <div className="first"></div>
+      <div className="first"></div>
+      </div>
+
+      <div className="row-one">
+      <div className="first"></div>
+      <div className="first"></div>
+      <div className="first"></div>
+      <div className="first"></div>
+      <div className="first"></div>
+      </div>
+
+      <div className="row-one">
+      <div className="first"></div>
+      <div className="first"></div>
+      <div className="first"></div>
+      <div className="first"></div>
+      <div className="first"></div>
+      </div>
+
+      <div className="row-one">
+      <div className="first"></div>
+      <div className="first"></div>
+      <div className="first"></div>
+      <div className="first"></div>
+      <div className="first"></div>
+      </div>
+
+      <div className="row-one">
+      <div className="first"></div>
+      <div className="first"></div>
+      <div className="first"></div>
+      <div className="first"></div>
+      <div className="first"></div>
+      </div>
+
+      <div className="row-one">
+      <div className="first"></div>
+      <div className="first"></div>
+      <div className="first"></div>
+      <div className="first"></div>
+      <div className="first"></div>
+      </div>
+      
+      
+ 
+
+
+
+        
+
+        <div className="wordsbtn">
+        
+            <button className="mybtn">Q</button>
+            <button className="mybtn">W</button>
+            <button className="mybtn">E</button>
+            <button className="mybtn">R</button>
+            <button className="mybtn">T</button>
+            <button className="mybtn">Y</button>
+            <button className="mybtn">U</button>
+            <button className="mybtn">I</button>
+            <button className="mybtn">O</button>
+            <button className="mybtn">P</button>
+           
+
+        </div>
+
+        <div>
+            <button className="mybtn">A</button>
+            <button className="mybtn">S</button>
+            <button className="mybtn">D</button>
+            <button className="mybtn">F</button>
+            <button className="mybtn">G</button>
+            <button className="mybtn">H</button>
+            <button className="mybtn">J</button>
+            <button className="mybtn">K</button>
+            <button className="mybtn">L</button>
+        </div>
+
+
+        <div>
+            <button className="mybtn">ENTER</button>
+            <button className="mybtn">Z</button>
+            <button className="mybtn">X</button>
+            <button className="mybtn">C</button>
+            <button className="mybtn">V</button>
+            <button className="mybtn">B</button>
+            <button className="mybtn">N</button>
+            <button className="mybtn">M</button>
+            <button className="mybtn">CLEAR</button>
+         
+        </div>
+
+
+        
+
+    </div>
+    
+  );
+}
+
+export default App;
